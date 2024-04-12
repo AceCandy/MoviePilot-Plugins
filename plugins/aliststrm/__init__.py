187bec0d6da60aa284693b0fb366d04b00df0afe
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-04-12 10:55:29 +0800

    Update __init__.py

--- plugins/aliststrm/__init__.py
+++ plugins/aliststrm/__init__.py
@@ -154,7 +154,7 @@ class AlistStrm(_PluginBase):
         with open('directory_structure.json', 'w') as f:
             json.dump(json_structure, f, indent=4)
 
-def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
     return [
         {
             'component': 'VForm',
