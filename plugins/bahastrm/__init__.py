fa7955d59f424330d7edf0687434456a07b13539
AceCandy <804649325@qq.com>
2024-08-25 02:52:08 +0800

    更新版本号

--- plugins/bahastrm/__init__.py
+++ plugins/bahastrm/__init__.py
@@ -58,7 +58,7 @@ class BahaStrm(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/Akimio521/MoviePilot-Plugins/main/icons/bahastrm.png"
     # 插件版本
-    plugin_version = "2.3"
+    plugin_version = "1.0"
     # 插件作者
     plugin_author = "Akimio521"
     # 作者主页
