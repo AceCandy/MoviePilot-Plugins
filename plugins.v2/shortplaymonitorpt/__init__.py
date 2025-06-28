1e6a374c7534957447a9257fc3ba29186f24815b
AceCandy <804649325@qq.com>
2025-06-29 02:20:16 +0800

    1.0.4

--- plugins.v2/shortplaymonitorpt/__init__.py
+++ plugins.v2/shortplaymonitorpt/__init__.py
@@ -61,7 +61,7 @@ class ShortPlayMonitorPt(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/create.png"
     # 插件版本
-    plugin_version = "1.0.3"
+    plugin_version = "1.0.4"
     # 插件作者
     plugin_author = "AceCandy"
     # 作者主页
